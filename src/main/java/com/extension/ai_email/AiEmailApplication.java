c36799a952d163622d73dbc38d403fb67880d918
Umar Firoz <f.umar@op.iitg.ac.in>
2026-07-06 20:28:34 +0530

    inital push

---
+++ src/main/java/com/extension/ai_email/AiEmailApplication.java
@@ -0,0 +1,13 @@
+package com.extension.ai_email;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiEmailApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiEmailApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/extension/ai_email/AiEmailApplicationTests.java
@@ -0,0 +1,13 @@
+package com.extension.ai_email;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiEmailApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
