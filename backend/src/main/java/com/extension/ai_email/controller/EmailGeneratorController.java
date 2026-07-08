11fdbb3e0b97f024270e92ba50aa23b697e3e8dd
Umar Firoz <f.umar@op.iitg.ac.in>
2026-07-08 15:50:49 +0530

    Added chrome extension file

--- backend/src/main/java/com/extension/ai_email/controller/EmailGeneratorController.java
+++ backend/src/main/java/com/extension/ai_email/controller/EmailGeneratorController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/api/email")
 @RequiredArgsConstructor
-@CrossOrigin(origins = "http://localhost:5173")
+@CrossOrigin("*")
 public class EmailGeneratorController {
     private final EmailGeneratorService emailGeneratorService;
 
